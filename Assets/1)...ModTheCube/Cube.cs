a04401df73850c5e66aa3f03b187dab099ff66c2
Aryeh Mischel <81983169+VRSoup@users.noreply.github.com>
2022-08-17 21:27:05 -0700

    Initial commit

---
+++ Assets/1)...ModTheCube/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public float Position;
+    public float Scale;
+    private bool black;
+    private Material mat;
+    private Color startColor;
+    private Color endColor;
+
+    void Start()
+    {
+        startColor = Random.ColorHSV();
+        endColor = Random.ColorHSV();
+    }
+    
+    void Update()
+    {
+       
+       Color color = Renderer.material.color;
+       color.a = Mathf.Lerp(0, 1, Mathf.PingPong(Time.time, 2));
+       color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time, 1));
+       Renderer.material.color = color;
+
+    }
+}
---
+++ Assets/2)...CounterPrototype/Scripts/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+
+    private int Count = 0;
+    public Numbers Numbers;
+
+    private void Start()
+    {
+        Count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        other.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        Count += 1;
+        Numbers.input = Count.ToString();
+        Numbers.spawnnumbers();
+        
+    }
+}
